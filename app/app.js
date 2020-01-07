3d4ce4b336f4db243eecdf8c748d7873990b500d
ptkila <pt.kila@gmail.com>
2020-01-07 03:13:29 -0300

    basic app file

---
+++ app/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const routes = require('./routes');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/', routes.router);
+
+app.listen(3000);

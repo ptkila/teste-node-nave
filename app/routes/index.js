8e9a05bb3a41e05b45136123b40c897ddd48c00a
ptkila <pt.kila@gmail.com>
2020-01-09 00:23:21 -0300

    removed and reorganized routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,7 +4,8 @@ const application = require('./application');
 const job = require('./job');
 const login = require('./login');
 const logout = require('./logout');
-const register = require('./register');
+const registerAdmin = require('./registerAdmin');
+const registerCandidate = require('./registerCandidate');
 
 const AuthMiddleware = require('../middlewares/AuthMiddleware');
 
@@ -12,17 +13,19 @@ const AuthMiddleware = require('../middlewares/AuthMiddleware');
 
 router.use('/login', login.router);
 
+router.use('/register-candidate', registerCandidate.router);
+
 // Auth - ON
 
 router.use(AuthMiddleware.authenticateUser);
 
-router.use('/logout', logout.router);
-
-router.use('/application', application.router);
+router.use('/register-admin', registerAdmin.router);
 
 router.use('/job', job.router);
 
-router.use('/register', register.router);
+router.use('/application', application.router);
+
+router.use('/logout', logout.router);
 
 module.exports = {
     router: router

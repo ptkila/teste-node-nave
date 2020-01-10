50f3ee1664b67eeea8b19f870969c0c93cf28f8d
ptkila <pt.kila@gmail.com>
2020-01-10 02:58:42 -0300

    better returning and add basic validation

--- app/controllers/RegisterCandidateController.js
+++ app/controllers/RegisterCandidateController.js
@@ -1,33 +1,42 @@
 const CandidateRepository = require('../../repositories/CandidateRepository');
 const UserRepository = require('../../repositories/UserRepository');
 const helperMethods = require('../../helpers/methods');
-const reqNeededReqBody = ['name', 'password', 'password_confirmation', 'email', 'phone', 'cpf']; 
+const reqNeededParams = ['name', 'password', 'password_confirmation', 'email', 'phone', 'cpf'];
 
 const register = async (req, res) => {
 
-    // TODO: REQUEST VALIDATION
-    let reqBody = req.body;
+    let params = req.body;
 
-    if(! helperMethods.equalStrings(reqBody.password, reqBody.password_confirmation)) {
-        return res.json({message:'Passwords are different'});
+    if (!helperMethods.equalStrings(params.password, params.password_confirmation)) {
+        return res.status(422).json({
+            message: 'Passwords are different'
+        });
     }
 
     try {
-        reqBody.role = 'App\\Candidate';
-        var newUser = await UserRepository.create(reqBody);
-    } catch(err) {
-        return res.json({message: err.errors[0].message, error: err});
+        params.role = 'App\\Candidate';
+        var newUser = await UserRepository.create(params);
+    } catch (err) {
+        return res.status(500).json({
+            message: err.errors[0].message,
+            error: err
+        });
     }
 
     try {
-        reqBody.userId = newUser.id;
-        await CandidateRepository.create(reqBody);
+        params.userId = newUser.id;
+        await CandidateRepository.create(params);
     } catch (err) {
         await newUser.destroy();
-        return res.json({message: err.errors[0].message});
+        return res.status(500).json({
+            message: err.errors[0].message
+        });
     }
 
-    return res.json({message: 'Success creating new candidate', user: newUser});
+    return res.status(201).json({
+        message: 'success',
+        user: newUser
+    });
 }
 
 module.exports = {

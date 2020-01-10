d740e6d162c52aba57cd436d50ebed70beb84b1e
ptkila <pt.kila@gmail.com>
2020-01-10 00:47:32 -0300

    add routes for listing and add comment

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,6 +1,30 @@
 const router = require('express').Router();
+const ApplicationController = require('../controllers/ApplicationController');
+const ApplicationCommentController = require('../controllers/ApplicationCommentController');
 
-router.use('/comment', (req, res, next) => {
+router.get('/list', (req, res, next) => {
+    if (!req.admin) {
+        return ApplicationController.listCandidate(req, res);
+    }
+    return ApplicationController.listAdmin(req, res);
+});
+
+router.get('/:application_id/comments', (req, res, next) => {
+    if (!req.admin) {
+        return res.status(403).json({
+            message: 'only admin can access'
+        });
+    }
+    return ApplicationController.listApplicationComments(req, res);
+});
+
+router.post('/comment', (req, res, next) => {
+    if (!req.admin) {
+        return res.status(403).json({
+            message: 'only admin can access'
+        });
+    }
+    return ApplicationCommentController.create(req, res);
 });
 
 module.exports = {

cf35f4e70e14f8aa70ea635b2dfa1025699a8e4a
ptkila <pt.kila@gmail.com>
2020-01-10 00:42:43 -0300

    better returns and methods for apply and list

--- app/controllers/JobController.js
+++ app/controllers/JobController.js
@@ -1,23 +1,118 @@
 const ApplicationRepository = require('../../repositories/ApplicationRepository');
 const JobRepository = require('../../repositories/JobRepository');
-
-const helperMethods = require('../../helpers/methods');
+const CandidateRepository = require('../../repositories/CandidateRepository');
 
 const create = async (req, res) => {
+    if (!req.admin) {
+        return res.status(403).json({
+            message: 'only admin can access'
+        });
+    }
+
     const params = req.body;
     try {
         var job = await JobRepository.create(params);
-    } catch(err) {
-        return res.json({message: err.message});
+    } catch (err) {
+        return res.status(500).json({
+            message: err.message
+        });
     }
-    return res.json({message:'success', job: job});
+    return res.status(200).json({
+        message: 'success',
+        job: job
+    });
 }
 
 const apply = async (req, res) => {
-    return res.json({message:'apply'});
+
+    if (req.admin) {
+        return res.status(403).json({
+            message: 'only candidate can access'
+        });
+    }
+
+    const params = req.body;
+
+    try {
+        var candidate = await CandidateRepository.find(params.candidateId);
+    } catch (err) {
+        return res.status(500).json({
+            message: err.message
+        });
+    }
+
+    if (!candidate) {
+        return res.status(404).json({
+            message: 'candidate not found'
+        });
+    }
+
+    if (candidate.userId != req.userId) {
+        return res.status(403).json({
+            message: 'candidate invalid'
+        });
+    }
+
+    try {
+        var jobExists = await JobRepository.find(params.jobId);
+    } catch (err) {
+        return res.status(500).json({
+            message: err.message
+        });
+    }
+
+    if (!jobExists) {
+        return res.status(404).json({
+            message: 'Job not found'
+        });
+    }
+
+    try {
+        var applicationExists = await ApplicationRepository.findByCandidateAndJob(params.candidateId, params.jobId);
+    } catch (err) {
+        return res.status(500).json({
+            message: err.message
+        });
+    }
+
+    if (applicationExists) {
+        return res.status(200).json({
+            message: 'already applied to job'
+        });
+    }
+
+    try {
+        var application = await ApplicationRepository.create(params);
+    } catch (err) {
+        return res.status(500).json({
+            message: err.message
+        });
+    }
+
+    return res.status(201).json({
+        message: 'success',
+        application: application
+    });
+
+}
+
+const list = async (req, res) => {
+    try {
+        var jobs = await JobRepository.list();
+    } catch (err) {
+        return res.status(500).json({
+            message: err.message
+        });
+    }
+
+    return res.status(200).json({
+        message: 'success',
+        jobs: jobs
+    });
 }
 
 module.exports = {
-    apply: apply, 
-    create: create, 
+    apply: apply,
+    create: create,
+    list: list, 
 }

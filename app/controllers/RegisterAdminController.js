15cf98e953402e6b6efaa2d581282dcdafe8ed9c
ptkila <pt.kila@gmail.com>
2020-01-10 00:45:54 -0300

    better returns and naming

--- app/controllers/RegisterAdminController.js
+++ app/controllers/RegisterAdminController.js
@@ -1,33 +1,42 @@
 const AdminRepository = require('../../repositories/AdminRepository');
 const UserRepository = require('../../repositories/UserRepository');
 const helperMethods = require('../../helpers/methods');
-const reqNeededReqBody = ['name', 'password', 'password_confirmation', 'email']; 
+const reqNeededParams = ['name', 'password', 'password_confirmation', 'email'];
 
 const register = async (req, res) => {
 
     // TODO: REQUEST VALIDATION
-    let reqBody = req.body;
+    let params = req.body;
 
-    if(! helperMethods.equalStrings(reqBody.password, reqBody.password_confirmation)) {
-        return res.json({message:'Passwords are different'});
+    if (!helperMethods.equalStrings(params.password, params.passwordConfirmation)) {
+        return res.status(200).json({
+            message: 'Passwords are different'
+        });
     }
 
     try {
-        reqBody.role = 'App\\Admin';
-        var newUser =  await UserRepository.create(reqBody);
-    } catch(err) {
-        return res.json({message: err.errors[0].message});
+        params.role = 'App\\Admin';
+        var newUser = await UserRepository.create(params);
+    } catch (err) {
+        return res.status(500).json({
+            message: err.errors[0].message
+        });
     }
 
     try {
-        reqBody.userId = newUser.id;
-        await AdminRepository.create(reqBody);
+        params.userId = newUser.id;
+        await AdminRepository.create(params);
     } catch (err) {
         await newUser.destroy();
-        return res.json({message: err.errors[0].message});
+        return res.status(500).json({
+            message: err.errors[0].message
+        });
     }
 
-    return res.json({message: 'Success creating new admin', user: newUser});
+    return res.status(201).json({
+        message: 'success',
+        user: newUser
+    });
 }
 
 module.exports = {

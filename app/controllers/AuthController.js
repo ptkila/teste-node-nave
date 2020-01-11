5987f121677d952d4df1a2356950b35f8ecccfbf
ptkila <pt.kila@gmail.com>
2020-01-10 23:55:03 -0300

    return the role of the user and better returns

--- app/controllers/AuthController.js
+++ app/controllers/AuthController.js
@@ -1,15 +1,40 @@
 const AuthService = require('../../services/AuthService');
+const CandidateRepository = require('../../repositories/CandidateRepository');
+const AdminRepository = require('../../repositories/AdminRepository');
 
 const login = async (req, res) => {
-    const response = await AuthService.login(req.body);
-    return res.json(response);
+    var response = await AuthService.login(req.body);
+    if (response.error == 1) {
+        return res.status(404).json({
+            message: response.message
+        });
+    } else if (response.error == 2) {
+        return res.status(500).json({
+            message: response.message
+        });
+    } else {
+        if (response.user.role == 'App\\Admin') {
+            var admin = await AdminRepository.findByUserId(response.user.id);
+            response.admin = admin;
+        } else {
+            var candidate = await CandidateRepository.findByUserId(response.user.id);
+            response.candidate = candidate;
+        }
+        return res.status(200).json(response);
+    }
 }
 
 const logout = async (req, res) => {
-    await AuthService.logout(req.user);
-    return res.json({
-        message: 'Logged out successfully'
-    });
+    const response = await AuthService.logout(req.user);
+    if (response.error == 0) {
+        return res.status(204).json({
+            message: response.message
+        });
+    } else {
+        return res.status(500).json({
+            message: response.message
+        });
+    }
 }
 
 module.exports = {

69310a4e8283d98f5e41ec2e1b3852a9a0f5437a
ptkila <pt.kila@gmail.com>
2020-01-10 23:59:53 -0300

    add route do documentation

--- app/app.js
+++ app/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
+const docsPath = path.resolve(__dirname, '../documentation');
 
 const routes = require('./routes');
 
@@ -7,6 +9,12 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+/**
+ * Documentação
+ * url: http://localhost:3000/
+ */
+app.use(express.static(docsPath));
 
 app.use('/', routes.router);
 
